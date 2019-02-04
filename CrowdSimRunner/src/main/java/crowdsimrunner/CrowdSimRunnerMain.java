b6d55535dda286b83ba1909f2b92d6afde88b63f
Nick Malleson <n.s.malleson@leeds.ac.uk>
2019-02-04 22:45:10 +0000

    created corridor and have agents starting at regular intervals, spread evenly across corridor. Broken for some reaosn, maybe to do with distribution of agents in vertical. Need to debug

--- CrowdSimRunner/src/main/java/crowdsimrunner/CrowdSimRunnerMain.java
+++ CrowdSimRunner/src/main/java/crowdsimrunner/CrowdSimRunnerMain.java
@@ -25,16 +25,23 @@ import java.util.List;
 
 
 /**
- * Custom class to run the Crowd Simulator
+ * Custom class to run the Crowd Simulator. It creates a rectangular area H meters high by W wide. Agents are
+ * spawned on the left and through waypoints to the other end.
  */
 public class CrowdSimRunnerMain extends CrowdSimulation {
 
-    private static int NumAgents = 100; // Number of agents to create when not reading data
-    private static int NumWaypoints = 5; // Number of waypoints
+    private static int NumAgents = 10; // Number of agents to create each spawn time
+    private static int createInterval = 1; // Interval between creating agents (in seconds)
+    private static int numIntervals = 100; // Number of times to spawn new agents
+
+    private static int WIDTH = 50;
+    private static int HEIGHT= 10;
 
     private static Route route = null; // All agents will follw the same route
 
     private static final GeometryFactory geomFac = new GeometryFactory();
+
+
 
     enum DATA {
         BERLIN,
@@ -64,11 +71,11 @@ public class CrowdSimRunnerMain extends CrowdSimulation {
         System.out.println("... simulation started");
 
         // Every 10 seconds, add more people, and do this 10 times
-        for (int i = 0; i<10 ; i++) {
+        for (int i = 0; i<CrowdSimRunnerMain.numIntervals ; i++) {
             try {
-                System.out.print("\tSleeping for 10 seconds...");
-                Thread.sleep(10000);
-                System.out.println("... woken up");
+                //System.out.print("\tSleeping for "+createInterval+" seconds...");
+                Thread.sleep(createInterval* 1000);
+                //System.out.println("... woken up");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -124,12 +131,11 @@ public class CrowdSimRunnerMain extends CrowdSimulation {
                     CoordinateReferenceSystem crs = CRS.parseWKT("PROJCS[\"OSGB 1936 / British National Grid\",GEOGCS[\"OSGB 1936\",DATUM[\"OSGB_1936\",SPHEROID[\"Airy 1830\",6377563.396,299.3249646,AUTHORITY[\"EPSG\",\"7001\"]],AUTHORITY[\"EPSG\",\"6277\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.01745329251994328,AUTHORITY[\"EPSG\",\"9122\"]],AUTHORITY[\"EPSG\",\"4277\"]],UNIT[\"metre\",1,AUTHORITY[\"EPSG\",\"9001\"]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"latitude_of_origin\",49],PARAMETER[\"central_meridian\",-2],PARAMETER[\"scale_factor\",0.9996012717],PARAMETER[\"false_easting\",400000],PARAMETER[\"false_northing\",-100000],AUTHORITY[\"EPSG\",\"27700\"],AXIS[\"Easting\",EAST],AXIS[\"Northing\",NORTH]]");
                     super.crowdSimulator.setCrs(crs);
 
-                    // Initialise the waypoints, these are used by all crowds
+                    // Initialise the waypoints, these are used by all crowds.
+                    // Make one waypoint near the beginning and another at the end of the corridor
                     List<Geometry> waypoints = new ArrayList<>();
-                    //for (int i = 0; i < CrowdSimRunnerMain.NumWaypoints; i++) {
-                    waypoints.add(geomFac.createPoint(new Coordinate(20.0, 10.0 )));
-                    waypoints.add(geomFac.createPoint(new Coordinate(50.0, 10.0 )));
-                    //}
+                    waypoints.add(geomFac.createPoint(new Coordinate(Math.round(WIDTH/3), Math.round(HEIGHT/2))));
+                    waypoints.add(geomFac.createPoint(new Coordinate(Math.round(WIDTH-1), Math.round(HEIGHT/2))));
                     CrowdSimRunnerMain.route = super.crowdSimulator.getRouteFactory().createRouteFromGeometries(waypoints);
 
 
@@ -145,20 +151,31 @@ public class CrowdSimRunnerMain extends CrowdSimulation {
                     }
 
 
-                    // LOAD BOUNDARIES. Just one big polygon for now
-                   /* List<Geometry> boundaries = new ArrayList<Geometry>();
-                    boundaries.add(geomFac.createPolygon(new Coordinate[]{
-                            new Coordinate(0,0),
-                            new Coordinate(60,0),
-                            new Coordinate(60,30),
-                            new Coordinate(0,30),
-                            new Coordinate(0,0)
+                    // LOAD BOUNDARIES. One big corridor now
+                    List<Geometry> boundaries = new ArrayList<>();
+                    /*boundaries.add(geomFac.createLineString(new Coordinate[]{ // Line along the bottom
+                            new Coordinate(0, 0),
+                            new Coordinate(WIDTH, 0)
                     }));
-                    crowdSimulator.addBoundaries(boundaries, false);
+                    boundaries.add(geomFac.createLineString((new Coordinate[]{ // Line along the top
+                            new Coordinate(0, HEIGHT),
+                            new Coordinate(WIDTH, HEIGHT)
+                    })));
+                    boundaries.add(geomFac.((new Coordinate[]{ // Line along the left
+                            new Coordinate(0, 0),
+                            new Coordinate(0, HEIGHT)
+                    })));*/
+                    boundaries.add(geomFac.createPolygon(new Coordinate[] {
+                            new Coordinate(0, 0),
+                            new Coordinate(WIDTH, 0),
+                            new Coordinate(WIDTH, HEIGHT),
+                            new Coordinate(0, HEIGHT),
+                            new Coordinate(0, 0)
+                    }));
+                    super.crowdSimulator.addBoundaries(boundaries, false);
 
                     // set bounding box
-                    crowdSimulator.expandBoundingBox(GeometryTools.getEnvelope(boundaries));
-                    */
+                    super.crowdSimulator.expandBoundingBox(GeometryTools.getEnvelope(boundaries));
 
                     // paint
                     if (map != null)
@@ -203,8 +220,32 @@ public class CrowdSimRunnerMain extends CrowdSimulation {
 
         // create a list of Geometry objects to represent the people
         List<Geometry> people = new ArrayList<>();
+        // Create people spread evenly vertically within the corridor
         for (int i = 0; i < CrowdSimRunnerMain.NumAgents; i++) {
-            people.add(geomFac.createPoint(new Coordinate(1.0, 1.0+(i*0.1))));
+            /*double y;
+            if (i==0) {
+                y = Double.MIN_VALUE; // avoid divide by 0
+            }
+            else if (i==CrowdSimRunnerMain.NumAgents-1) {
+                y = HEIGHT-Double.MIN_VALUE; // not quite on the upper corridor
+            }
+            else {
+                y = HEIGHT * ((float)i/(CrowdSimRunnerMain.NumAgents-1)); // Agents spread evenly vertically
+            }*/
+            // Somewhere randomly 1m between the upper and lower walls
+            // double y = (Math.random()*HEIGHT - 1) / (HEIGHT - 1);
+            // Spread evenly vertically 1m inside the upper and lower walls
+            //double y = ( ((double)(i+1.0)/CrowdSimRunnerMain.NumAgents)*HEIGHT - 1.0 ) / (HEIGHT - 1.0);
+
+            // Complicated way to calculate agent position, scaling to within 1 m of the upper and lower boundaries
+            // (from https://stackoverflow.com/questions/5294955/how-to-scale-down-a-range-of-numbers-with-a-known-min-and-max-value)
+            double pos = ((i+1)/(double)CrowdSimRunnerMain.NumAgents); // (called 'x' on the website above)
+            double a = 1; // min value we want to scale to
+            double b = HEIGHT-1; // max value we want to scale to
+            double y = ( ((b-a)*(pos-0)) / (1-0) ) + a;
+            System.out.println(y);
+
+            people.add(geomFac.createPoint(new Coordinate(5.0, y )));
         }
 
         // create crowd object
