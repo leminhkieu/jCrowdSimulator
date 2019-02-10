6676a4384c8085c7814c6a25c8896ca584004c04
Nick Malleson <n.s.malleson@leeds.ac.uk>
2019-02-10 21:45:22 +0000

    updating pedestrians to allow them to know when to leave the simulation

---
+++ CrowdSimRunner/src/main/java/crowdsimrunner/NewPedestrian.java
@@ -0,0 +1,10 @@
+package crowdsimrunner;
+
+public class NewPedestrian extends Pedestrian {
+
+    @Override
+    public void move(long time, double simulationInterval) {
+        super.move(time, simulationInterval);
+        // TODO Add stuff here that can be called each time this pedestrian moves
+    }
+}
